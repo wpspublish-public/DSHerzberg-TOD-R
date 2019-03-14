8d5e58e8e287307a39598d7e095f05b5d4af614d
David Herzberg <dherzberg@wpspublish.com>
2019-03-13 17:26:30 -0700

    creating reprex

--- 2-level-strat-track.R
+++ 2-level-strat-track.R
@@ -9,20 +9,18 @@ library(magrittr) # PIPE OPERATORS
 suppressMessages(suppressWarnings(library(tidyverse)))
 suppressMessages(library(ggpmisc)) # EXTENSIONS TO ggplot2: ADD EQUATIONS AND FIT STATISTICS TO FITTED LINE PLOTS
 library(ggrepel) # MORE ggplot2 EXTENSIONS
+library(forcats) # FACTOR TOOLS
 
 
-TOD_demos <- suppressMessages(read_csv(here('DATA/TOD-demos_input1.csv'))) %>% arrange(Ageyear) %>% 
-  mutate(Agestrat = case_when(
-    Ageyear <=24 ~ as.character(Ageyear),
-    Ageyear >=25 & Ageyear <=40 ~ "25-40",
-    Ageyear >=41 & Ageyear <=50 ~ "41-50",
-    Ageyear >=51 & Ageyear <=60 ~ "51-60",
-    Ageyear >=61 & Ageyear <=70 ~ "61-70",
-    Ageyear >=71 & Ageyear <=80 ~ "71-80",
-    Ageyear >=81 & Ageyear <=90 ~ "81-90",
+TOD_demos <- suppressMessages(read_csv(here('DATA/TOD-demos_input1.csv'))) %>% arrange(ageyear) %>% 
+  mutate(agestrat = case_when(
+    ageyear <=24 ~ as.character(ageyear),
+    ageyear >=25 & ageyear <=40 ~ "25-40",
+    ageyear >=41 & ageyear <=50 ~ "41-50",
+    ageyear >=51 & ageyear <=60 ~ "51-60",
+    ageyear >=61 & ageyear <=70 ~ "61-70",
+    ageyear >=71 & ageyear <=80 ~ "71-80",
+    ageyear >=81 & ageyear <=90 ~ "81-90",
     TRUE ~ NA_character_
-  )) %>% group_by(Agestrat)
-  
-  
-
+  )) %>% mutate_at(vars(agestrat), as.factor) %>% group_by(agestrat)
 
