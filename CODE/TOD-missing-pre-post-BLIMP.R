470ac8ae0de596077ebf8892f4cb378330ea1dcb
David Herzberg <dherzberg@wpspublish.com>
2020-03-28 17:04:08 -0700

    make code more efficient and elegant

--- CODE/TOD-missing-pre-post-BLIMP.R
+++ CODE/TOD-missing-pre-post-BLIMP.R
@@ -3,16 +3,16 @@
 suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 
-id <- c('ID')
-first_item <- c('i001')
-last_item <- c('i534')
-file_name <- c('TOD-E-missing-2020-03-05')
+file_name <- c('TOD-E-missing-2020-03-05-250var')
 
 input_orig <- suppressMessages(read_csv(here(
   paste0('INPUT-FILES/', file_name, '.csv')
-))) %>% 
-  select(id, first_item:last_item)
-names_input_orig <- names(input_orig)
+))) 
+
+# get problematic columns, rows
+all_0orNA_cols <- input_orig %>% purrr::keep(~all(is.na(.x) | .x == 0)) %>% names
+all_1orNA_cols <- input_orig %>% purrr::keep(~all(is.na(.x) | .x == 1)) %>% names
+all_0orNA_rows <- input_orig %>% filter_at(names(input_orig)[-1], ~(is.na(.) | . == 0))
 
 NA_count <- sum(is.na(input_orig))
 NA_count
@@ -21,11 +21,32 @@ input_orig[is.na(input_orig)] <- 999
 
 input_gathered <- input_orig %>%
   gather('item','response',-id) %>% 
-  group_by(!!sym(id)) %>% 
-  arrange(!!sym(id)) %>% 
+  group_by(!!sym(names(input_orig)[1])) %>% 
+  arrange(!!sym(names(input_orig)[1])) %>% 
   mutate(item = as.factor(str_sub(item, 2, 4)))
 
 write_csv(input_gathered,
           here(paste0(file_name, '-BLIMP-input.csv')),
           col_names = F
 )
+
+
+# reformat imputed data set for downstream analysis
+temp1 <- suppressMessages(
+  read_csv(
+    (here('TOD-impute1.csv')), col_names = F))
+names(temp1) <- c('id', 'item', 'response')
+temp2 <- temp1 %>% 
+  spread(item, response) 
+names(temp2) <- names(input_orig)
+
+NA_count <- sum(temp2 == 999)
+NA_count
+
+write_csv(temp2, here(
+  paste0(
+    'OUTPUT-FILES/',
+    file_name,
+    '-noMiss.csv'
+  )
+))
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,6 +1,23 @@
-# TOD: two-level demographic tracking
+suppressMessages(library(tidyverse))
 
-suppressMessages(library(here))
-suppressMessages(suppressWarnings(library(data.table)))
-suppressMessages(suppressWarnings(library(tidyverse)))
+input <- tribble(
+  ~id, ~item1, ~item2, ~item3, ~item4,
+  'A', 0, 0, NA, NA, 
+  'B', 0, 0, 0, 0, 
+  'C', NA, 0, 1, 0, 
+  'D', 1, 0, 1, 1,
+  'E', 0, NA, 0, 0
+)
+
+output <- tribble(
+  ~id, ~item1, ~item2, ~item3, ~item4,
+  'A', 0, 0, NA, NA, 
+  'B', 0, 0, 0, 0, 
+  'E', 0, NA, 0, 0
+)
+
+items <- c(names(input))[-1]
+
+all_0orNA_rows <- input %>% filter_at(names(input)[-1], ~(is.na(.) | . == 0))
+identical(all_0orNA_rows, output)
 
