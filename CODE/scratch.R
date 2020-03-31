97bbc7cb979f61c327d2f04fa6190ec99c31afe7
David Herzberg <dherzberg@wpspublish.com>
2020-03-31 10:06:52 -0700

    Reality check on TOD-BLIMP estimtes

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,23 +1,33 @@
+suppressMessages(library(here))
 suppressMessages(library(tidyverse))
+suppressMessages(library(psych)) # DESCRIPTIVE TABLES
 
-input <- tribble(
-  ~id, ~item1, ~item2, ~item3, ~item4,
-  'A', 0, 0, NA, NA, 
-  'B', 0, 0, 0, 0, 
-  'C', NA, 0, 1, 0, 
-  'D', 1, 0, 1, 1,
-  'E', 0, NA, 0, 0
-)
+TOD_comp<- suppressMessages(read_csv(here(
+  'INPUT-FILES/TOD-E_PVandLW_BLIMPcompare3.5.20.csv'
+))) 
 
-output <- tribble(
-  ~id, ~item1, ~item2, ~item3, ~item4,
-  'A', 0, 0, NA, NA, 
-  'B', 0, 0, 0, 0, 
-  'E', 0, NA, 0, 0
-)
+TOD_comp_desc <-
+  TOD_comp %>% 
+  select(contains('sum')) %>% 
+  describe(fast = T) %>%
+  rownames_to_column() %>% 
+  rename(scale = rowname) %>% 
+  select(scale, n, mean, sd) %>% 
+  mutate_at(vars(mean, sd), ~(round(., 2)))
 
-items <- c(names(input))[-1]
+TOD_comp_desc <-
+  TOD_comp %>% 
+  select(contains('sum')) %>%
+  cor()
+  
+# write_csv(Adult_Other_raw_desc, here(
+#   paste0(
+#     'OUTPUT-FILES/ADULT/DESCRIPTIVES/Adult-Other-raw-desc-',
+#     format(Sys.Date(), "%Y-%m-%d"),
+#     '.csv'
+#   )
+# ), 
+# na = ''
+# )
 
-all_0orNA_rows <- input %>% filter_at(names(input)[-1], ~(is.na(.) | . == 0))
-identical(all_0orNA_rows, output)
-
+glimpse(TOD_comp)
