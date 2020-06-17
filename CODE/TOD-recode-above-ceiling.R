258b1b3c353f2445e9e877d4d0c614f3807c62d3
David Herzberg <dherzberg@wpspublish.com>
2020-06-16 17:17:23 -0700

    set up documentation for recode above ceiling proc

--- CODE/TOD-recode-above-ceiling.R
+++ CODE/TOD-recode-above-ceiling.R
@@ -3,6 +3,15 @@ suppressMessages(library(tidyverse))
 suppressMessages(library(psych))
 suppressMessages(library(runner))
 
+urlRemote_path  <- "https://raw.githubusercontent.com/"
+github_path <- "DSHerzberg/TOD-R/master/INPUT-FILES/"
+fileName_path   <- "unweighted-input.csv"
+
+unweighted_input <- suppressMessages(read_csv(url(
+  str_c(urlRemote_path, github_path, fileName_path)
+)))
+
+
 input <- suppressMessages(
   read_csv(
     here(
@@ -11,46 +20,74 @@ input <- suppressMessages(
   )
 ) 
 
-recode_output <- input %>%
-  group_by(ID) %>%
+input_tall <- input %>%
   pivot_longer(
     cols = -ID,
     names_to = c("pre", "num"),
     names_sep = 4
-  ) %>%
-  mutate(
-    NA_status = case_when(
-      lead(num) == 1 & is.na(value) ~ "offset_NA",
-      is.na(value) & !is.na(lag(value)) & pre == lag(pre) ~ "onset_NA",
-      T ~ NA_character_
+  )  %>%
+  mutate(across(
+    c(pre),
+    ~ case_when(
+      pre == "lske" & between(num, 1, 15) ~ "lske_A",
+      pre == "lske" & between(num, 16, 25) ~ "lske_B",
+      pre == "lske" & between(num, 26, 35) ~ "lske_C",
+      T ~ pre
     )
-  ) %>%
-  group_by(ID, grp = cumsum(!is.na(NA_status))) %>%
-  mutate(NA_status = replace(NA_status, first(NA_status) == 'onset_NA', 'onset_NA')) %>%  
-  ungroup() %>%
+  )) %>%
+  group_by(ID, pre) %>%
   mutate(
-    across(
-         c(NA_status),
-         ~ case_when(
-           NA_status == "offset_NA" & is.na(lag(NA_status)) ~ NA_character_,
-           NA_status == "onset_NA" & ID != lead(ID) ~ "offset_NA", 
-           T ~ NA_status
-                     )
-       )
-  ) %>%
-  select(-grp) %>%
-  mutate(new_val = case_when(NA_status %in% c("onset_NA", "offset_NA") ~ 0,
-                             T ~ value)) %>%
-  pivot_wider(
-    id_cols = ID,
-    names_from = c(pre, num),
-    names_sep = "",
-    values_from = new_val
+    streak_val = case_when(value == 0 ~ streak_run(value, na_rm = F),
+                           T ~ NA_integer_),
+    ceiling = case_when((
+      pre %in% c("snwe", "sege", "lswe", "rhme") & streak_val == 5
+    ) |
+      (
+        pre %in% c("lske_A", "lske_B", "lske_C") & streak_val == 3
+      ) ~ 1,
+    T ~ 0)
   )
 
-write_csv(recode_output,
-          here(str_c("OUTPUT-FILES/TOD-E-recode-",
-                       format(Sys.Date(), "%Y-%m-%d"),
-                     ".csv")),
-          na = "")
+ceiling <-  input_tall %>% 
+  group_by(ID, pre) %>% 
+  summarise(ceiling_count = sum(ceiling)) %>% 
+  mutate(ceiling_reached = case_when(
+    ceiling_count >= 1 ~ 1,
+    T ~ NA_real_
+  )) %>% 
+  select(-ceiling_count)
+
+ recode_output <- input_tall %>% 
+  left_join(ceiling, by = c("ID", "pre")) %>% 
+  group_by(ID) %>%
+  mutate(
+    NA_status = case_when(
+      (pre != lead(pre) | is.na(lead(pre))) & is.na(value) & ceiling_reached == 1 ~ "offset_NA",
+      is.na(value) & !is.na(lag(value)) & pre == lag(pre) & ceiling_reached == 1 ~ "onset_NA",
+      T ~ NA_character_
+    )
+  ) %>% 
+   group_by(ID, pre) %>% 
+   mutate(
+     across(
+       c(NA_status),
+       ~ fill_run(.)
+     )
+   ) %>% 
+ mutate(new_val = case_when(NA_status %in% c("onset_NA", "offset_NA") ~ 0,
+                            T ~ value)) %>%
+   pivot_wider(
+     id_cols = ID,
+     names_from = c(pre, num),
+     names_sep = "",
+     values_from = new_val
+   )
+ 
+ write_csv(recode_output,
+           here(str_c("OUTPUT-FILES/TOD-E-recode-",
+                      format(Sys.Date(), "%Y-%m-%d"),
+                      ".csv")),
+           na = "")
+ 
+    
 
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -12,29 +12,33 @@ input <- suppressMessages(
 ) 
 
 input_tall <- input %>%
-  group_by(ID) %>%
+  # group_by(ID) %>%
   pivot_longer(
     cols = -ID,
     names_to = c("pre", "num"),
     names_sep = 4
   )  %>%
-  mutate(streak_val = case_when(value == 0 ~ streak_run(value, na_rm = F),
-                                T ~ NA_integer_),
-         across(
-           c(pre),
-           ~ case_when(
-             pre == "lske" & between(num, 1, 15) ~ "lske_A",
-             pre == "lske" & between(num, 16, 25) ~ "lske_B",
-             pre == "lske" & between(num, 26, 35) ~ "lske_C",
-             T ~ pre
-           )
-         ),
-         ceiling = case_when(
-           (pre %in% c("snwe", "sege", "lswe", "rhme") & streak_val == 5) | 
-             (pre %in% c("lske_A", "lske_B", "lske_C") & streak_val == 3) ~ 1,
-           T ~ 0
-         )
-         ) 
+  mutate(across(
+    c(pre),
+    ~ case_when(
+      pre == "lske" & between(num, 1, 15) ~ "lske_A",
+      pre == "lske" & between(num, 16, 25) ~ "lske_B",
+      pre == "lske" & between(num, 26, 35) ~ "lske_C",
+      T ~ pre
+    )
+  )) %>%
+  group_by(ID, pre) %>%
+  mutate(
+    streak_val = case_when(value == 0 ~ streak_run(value, na_rm = F),
+                           T ~ NA_integer_),
+    ceiling = case_when((
+      pre %in% c("snwe", "sege", "lswe", "rhme") & streak_val == 5
+    ) |
+      (
+        pre %in% c("lske_A", "lske_B", "lske_C") & streak_val == 3
+      ) ~ 1,
+    T ~ 0)
+  )
 
 ceiling <-  input_tall %>% 
   group_by(ID, pre) %>% 
@@ -42,7 +46,40 @@ ceiling <-  input_tall %>%
   mutate(ceiling_reached = case_when(
     ceiling_count >= 1 ~ 1,
     T ~ NA_real_
-  ))
+  )) %>% 
+  select(-ceiling_count)
 
+ recode_output <- input_tall %>% 
+  left_join(ceiling, by = c("ID", "pre")) %>% 
+  group_by(ID) %>%
+  mutate(
+    NA_status = case_when(
+      (pre != lead(pre) | is.na(lead(pre))) & is.na(value) & ceiling_reached == 1 ~ "offset_NA",
+      is.na(value) & !is.na(lag(value)) & pre == lag(pre) & ceiling_reached == 1 ~ "onset_NA",
+      T ~ NA_character_
+    )
+  ) %>% 
+   group_by(ID, pre) %>% 
+   mutate(
+     across(
+       c(NA_status),
+       ~ fill_run(.)
+     )
+   ) %>% 
+ mutate(new_val = case_when(NA_status %in% c("onset_NA", "offset_NA") ~ 0,
+                            T ~ value)) %>%
+   pivot_wider(
+     id_cols = ID,
+     names_from = c(pre, num),
+     names_sep = "",
+     values_from = new_val
+   )
+ 
+ write_csv(recode_output,
+           here(str_c("OUTPUT-FILES/TOD-E-recode-",
+                      format(Sys.Date(), "%Y-%m-%d"),
+                      ".csv")),
+           na = "")
+ 
     
 
