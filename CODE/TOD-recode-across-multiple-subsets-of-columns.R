43803fffca86b5529dbcda40390cf9fee2406d6e
David Herzberg <dherzberg@wpspublish.com>
2020-07-20 13:56:07 -0700

    update code for recode across multiple cols

--- CODE/TOD-recode-across-multiple-subsets-of-columns.R
+++ CODE/TOD-recode-across-multiple-subsets-of-columns.R
@@ -1,109 +1,49 @@
 suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 
-file_name <- c("TOD.DATA.3.5.20_forBLIMP7.16.20iws_lvc")
+file_name <- "TOD.DATA.3.5.20_forBLIMP7.16.20iws_lvc"
 
 input_orig <- suppressMessages(read_csv(here(
-  paste0("INPUT-FILES/", file_name, ".csv")
+  str_c(
+  "INPUT-FILES/", file_name, ".csv"
+  )
 ))) 
 
-temp1 <- suppressMessages(
+blimp_output <- suppressMessages(
   read_csv(
-    (here("MISSING-DATA-BLIMP/TOD-impute-2020-07-17-1.csv")), col_names = F))
-names(temp1) <- c("ID", "item", "response")
-temp2 <- temp1 %>% 
-  pivot_wider(
-    names_from = item,
-    values_from = response
-  ) 
-names(temp2) <- names(input_orig)
+    (here("MISSING-DATA-BLIMP/TOD-impute-2020-07-17-1.csv")), col_names = F)) %>% 
+  setNames(c("ID", "item", "response")) %>% 
+  pivot_wider(names_from = item,
+              values_from = response) %>%
+  setNames(names(input_orig))
 
-col_range <- c("i001:i050", "i051:i084", "i085:i114", "i115:i185", 
-               "i186:i206", "i207:i251", "i252:i293")
+col_subsets <- c("i001:i050", "i051:i084", "i085:i114", "i115:i185", 
+                 "i186:i206", "i207:i251", "i252:i293")
 
-# this section now has code to handle when a single case has two column ranges
-# that need to be recoded - these are now identified in recode_cols1 and recode_cols2
-miss_recode <- col_range %>% 
-  map_df(~
-           input_orig %>%
-           filter(across(!!rlang::parse_expr(.x),
-                         ~ is.na(.))) %>% 
-           mutate(recode_cols1 = .x) %>% 
-           select(ID, recode_cols1)
+miss_recode <- col_subsets %>%
+  map_df(
+    ~
+      input_orig %>%
+      filter(across(!!rlang::parse_expr(.x),
+                    ~ is.na(.))) %>%
+      mutate(recode_cols1 = .x) %>%
+      select(ID, recode_cols1)
+  ) %>%
+  arrange(ID) %>%
+  mutate(
+    streak = runner::streak_run(ID),
+    recode_cols2 = case_when(lead(streak) == 2 ~ lead(recode_cols1),
+                             T ~ NA_character_),
+    recode_cols3 = case_when(lead(streak, 2) == 3 ~ lead(recode_cols1, 2),
+                             T ~ NA_character_)
   ) %>% 
-  arrange(ID) %>% 
-  mutate(recode_cols2 = case_when(
-    lead(ID) == ID ~ lead(recode_cols1),
-    T ~ NA_character_
-  )) %>% 
-   filter(rownames(.) == "1" | lag(ID) != ID)
+  filter(streak == 1) %>% 
+  select(-streak)
 
-
-######### NEXT COMMENTED-OUT SNIPPET HAS NOT BEEN UPDATED TO HANDLE CASES WITH
-######### MORE THAN ONE MISSING COLUMN RANGE
-
-# # repeated use of across to recode over different subsets of columns
-# temp3 <- temp2 %>%
-#   left_join(miss_recode, by = "ID") %>%
-#   relocate(recode_cols1, .after = "ID") %>%
-#   mutate(
-#     across(
-#       c(i001:i050),
-#       ~ case_when(
-#         recode_cols1 == "i001:i050"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i051:i084),
-#       ~ case_when(
-#         recode_cols1 == "i051:i084"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i085:i114),
-#       ~ case_when(
-#         recode_cols1 == "i085:i114"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i115:i185),
-#       ~ case_when(
-#         recode_cols1 == "i115:i185"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i186:i206),
-#       ~ case_when(
-#         recode_cols1 == "i186:i206"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i207:i251),
-#       ~ case_when(
-#         recode_cols1 == "i207:i251"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     ), 
-#     across(
-#       c(i252:i293),
-#       ~ case_when(
-#         recode_cols1 == "i252:i293"  ~ NA_real_,
-#         T ~ .x
-#       )
-#     )
-#   ) %>%
-#   select(-recode_cols1)
-
-# pivot longer to avoid repeated code
-temp4 <- temp2 %>%
+blimp_recode <- blimp_output %>%
   left_join(miss_recode, by = "ID") %>%
-  relocate(c(recode_cols1, recode_cols2), .after = "ID") %>%
-  pivot_longer(cols = c(-ID, -recode_cols1,-recode_cols2),
+  relocate(c(recode_cols1, recode_cols2, recode_cols3), .after = "ID") %>%
+  pivot_longer(cols = c(-ID, -recode_cols1, -recode_cols2, -recode_cols3),
                names_to = c("item")) %>%
   extract(
     recode_cols1,
@@ -117,6 +57,12 @@ temp4 <- temp2 %>%
     "([:alnum:]{4})?\\:?(.*)",
     remove = F
   ) %>%
+  extract(
+    recode_cols3,
+    into = c("start3", "end3"),
+    "([:alnum:]{4})?\\:?(.*)",
+    remove = F
+  ) %>%
   group_by(ID) %>%
   mutate(
     recode_run =
@@ -125,13 +71,15 @@ temp4 <- temp2 %>%
         end1 == item ~ "recode1",
         start2 == item ~ "recode2",
         end2 == item ~ "recode2",
+        start3 == item ~ "recode3",
+        end3 == item ~ "recode3",
         T ~ NA_character_
       ),
     across(c(recode_run),
            ~ runner::fill_run(., only_within = T)),
     across(c(value),
            ~ case_when(
-             recode_run %in% c("recode1", "recode2") ~ NA_real_,
+             recode_run %in% c("recode1", "recode2", "recode3") ~ NA_real_,
              T ~ value
            ))
   ) %>%
@@ -139,9 +87,9 @@ temp4 <- temp2 %>%
   pivot_wider(
     names_from = item,
     values_from = value) %>%
-  unnest()
+  ungroup()
 
-write_csv(temp4, here(
+write_csv(blimp_recode, here(
   str_c(
     "MISSING-DATA-BLIMP/",
     file_name,
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,11 +1,14 @@
-# Check for any dupIDs (anyDuplicated() returns row number of FIRST dup ID encountered)
-anyDuplicated(temp4$ID)
+test <- miss_recode %>% 
+  left_join(input_orig, by = "ID")
 
-# Check for any NAs on IDNumber, returns TRUE if NA exist
-any(is.na(miss_recode$ID))
+test2 <- slice_sample(input_orig, n = 100)
 
-# extract cases with Dup ID numbers or NA on IDNumber, write out for investigation
-dupMissIDs <- miss_recode %>%
-  mutate(dup = duplicated(ID)) %>%
-  filter(dup == TRUE | is.na(ID)) %>%
-  select(-dup) 
+test3 <- bind_rows(test, test2) %>% 
+  arrange(ID) %>% 
+  filter(
+    !(ID %in% c(230010, 268005) & is.na(recode_cols1))
+  )
+
+anyDuplicated(test3$ID)
+
+write_csv(test3, here("test3.csv"))
