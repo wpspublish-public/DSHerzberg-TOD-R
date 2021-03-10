fc27d793c809fe83c63a27e97481eb59e2b82bd3
David Herzberg <dherzberg@wpspublish.com>
2021-03-10 10:17:19 -0800

    push test1 again

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,17 +1,8 @@
 urlRemote_path  <- "https://raw.githubusercontent.com/"
 github_path <- "DSHerzberg/TOD-R/master/INPUT-FILES/"
-fileName_path   <- "TODC-TODS2.23.21forceilingrecodes.csv"
+fileName_path   <- "test1.csv"
 
 input <- suppressMessages(read_csv(url(
-  str_c(urlRemote_path, github_path, fileName_path),
-  "rb"
+  str_c(urlRemote_path, github_path, fileName_path)
 )))
 
-con <- url("http://www.imdb.com/title/tt1490017/", "rb") 
-
-"https://raw.githubusercontent.com/DSHerzberg/TOD-R/master/INPUT-FILES/TODC-TODS2.23.21forceilingrecodes.csv"
-
-
-input <- suppressMessages(read_csv(url(
-  "https://raw.githubusercontent.com/DSHerzberg/TOD-R/master/INPUT-FILES/TODC-TODS2.23.21forceilingrecodes.csv"
-)))
