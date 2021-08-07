9aea232b8b8566ade0007b95097c2d85139ddcc6
David Herzberg <dherzberg@wpspublish.com>
2021-08-06 17:14:38 -0700

    build script for weighting TOD sample

--- CODE/DATA-WEIGHTING/TODCgr1-12-weighting-data-code.R
+++ CODE/DATA-WEIGHTING/TODCgr1-12-weighting-data-code.R
@@ -4,17 +4,19 @@ suppressMessages(suppressWarnings(library(survey)))
 
 set.seed(123)
 
-var_order <- c("Age", "Gender", "HighestEducation", "Ethnicity", "Region")
+var_order <- c("age", "gender", "educ", "ethnic", "region")
 
 var_order_census_match  <- c("gender", "educ", "ethnic", "region")
 
 cat_order <- c(
   NA, "6", "7", "8", "9", "10", "11", "12", "13", 
-      "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"
+      "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
   NA, "male", "female",
   NA, "no_HS", "HS_grad", "some_college", "BA_plus",
   NA, "hispanic", "asian", "black", "white", "other",
   NA, "northeast", "south", "midwest", "west")
+
+file_name <- "TODCgr1-12-"
 
 urlRemote_path  <- "https://raw.github.com/"
 github_path <- "wpspublish/DSHerzberg-TOD-R/master/INPUT-FILES/DATA-WEIGHTING/"
@@ -22,28 +24,43 @@ fileName_path   <- "TODCgr1-12-unweighted.csv"
 
 original_input <- suppressMessages(read_csv(url(
   str_c(urlRemote_path, github_path, fileName_path)
-)))
+))) %>% rename(
+  age = Age,
+  gender = Gender,
+  educ = HighestEducation,
+  ethnic = Ethnicity,
+) %>% 
+  filter(is.na(Noweight) & gender %in% c(1,2)) %>% 
+  mutate(
+    across(gender, ~ case_when(
+    . == 1 ~ "male",
+    . == 2 ~ "female")),
+    across(educ, ~ case_when(
+      . == 1 ~ "no_HS",
+      . == 2 ~ "HS_grad",
+      . == 3 ~ "some_college",
+      . == 4 ~ "BA_plus")),
+    across(ethnic, ~ case_when(
+      . == 1 ~ "asian",
+      . == 2 ~ "black",
+      . == 3 ~ "white",
+      . %in% c(4, 5, 6) ~ "other",
+      . == 7 ~ "hispanic")),
+    across(region, ~ case_when(
+      . == 1 ~ "northeast",
+      . == 2 ~ "midwest",
+      . == 3 ~ "south",
+      . == 4 ~ "west"))
+  )
 
-fileName_path   <- "data-input-sim.csv"
+fileName_path   <- "TODCgr1-12-census-pct.csv"
 
-census_match_input <- suppressMessages(read_csv(url(
+census_match_cat_count <- suppressMessages(read_csv(url(
   str_c(urlRemote_path, github_path, fileName_path)
-)))
+))) %>% 
+  mutate(n_census = round(nrow(original_input)*(pct_census/100), 0))
 
 rm(list = ls(pattern = "_path"))
-
-census_match_cat_count <- var_order_census_match %>%
-  map_df(
-    ~
-      census_match_input %>%
-      group_by(across(all_of(.x))) %>%
-      summarize(n_census = n()) %>%
-      rename(cat = all_of(.x)) %>%
-      mutate(var = all_of(.x),
-             pct_census = n_census/10) %>%
-      relocate(var, .before = cat)
-  ) %>% 
-  arrange(match(cat, cat_order))
 
 var_order_census_match %>%
   map(
@@ -72,7 +89,7 @@ input_demo_wts <- bind_cols(
 ) %>% 
   rename(samp_prob = rake_original_input...prob...) %>% 
   mutate(ratio = samp_prob / demo_wt) %>% 
-  select(ID:clin_status, samp_prob, demo_wt, ratio, everything()) %>% 
+  select(ID:region, samp_prob, demo_wt, ratio, everything()) %>% 
   arrange(desc(samp_prob))
 
 rm(list = ls(pattern = "object|rake"))
@@ -80,41 +97,45 @@ rm(list = ls(pattern = "object|rake"))
 unweighted_output <- input_demo_wts %>% 
   select(-c(samp_prob, ratio)) %>%
   rename_with(~ str_c("i", str_pad(
-    as.character(1:50), 2, side = "left", pad = "0"), "_uw"), 
-    i01:i50) %>% 
+    as.character(1:44), 2, side = "left", pad = "0"), "_uw"), 
+    i01:i44) %>% 
   mutate(
     TOT_raw_unweight = rowSums(.[grep("*_uw", names(.))]
   )) %>%
   relocate(TOT_raw_unweight, .after = demo_wt)
 
-write_csv(
-  unweighted_output,
-  here(
-    "OUTPUT-FILES/unweighted-data-for-analysis.csv" 
-  ),
-  na = ""
-)
+write_csv(unweighted_output,
+          here(
+            str_c(
+              "OUTPUT-FILES/DATA-WEIGHTING/",
+              file_name,
+              "unweighted-data-for-analysis.csv"
+            )
+          ),
+          na = "")
 
 weighted_output <- original_input %>%
   left_join(unweighted_output[c("ID", "demo_wt")], by = "ID") %>%
   rename_with(~ str_c("i", str_pad(
-    as.character(1:50), 2, side = "left", pad = "0"
+    as.character(1:44), 2, side = "left", pad = "0"
   ), "_w"),
-  i01:i50) %>%
-  mutate(across(c(i01_w:i50_w),
+  i01:i44) %>%
+  mutate(across(c(i01_w:i44_w),
                 ~ . * demo_wt)) %>%
   mutate(
     TOT_raw_weight = rowSums(.[grep("*_w$", names(.))]
     )) %>%
   relocate(demo_wt, TOT_raw_weight, .before = i01_w)
 
-write_csv(
-  weighted_output,
-  here(
-    "OUTPUT-FILES/weighted-data-for-analysis.csv" 
-  ),
-  na = ""
-)
+write_csv(weighted_output,
+          here(
+            str_c(
+              "OUTPUT-FILES/DATA-WEIGHTING/",
+              file_name,
+              "weighted-data-for-analysis.csv"
+            )
+          ),
+          na = "")
 
 # PROOF OF CONCEPT
 
@@ -127,7 +148,7 @@ cat_count_comp <-
       summarize(n_input = n()) %>%
       rename(cat = all_of(.x)) %>%
       mutate(var = all_of(.x),
-             pct_input = n_input / 10) %>%
+             pct_input = round((n_input / nrow(original_input)) * 100, 1)) %>%
       relocate(var, .before = cat)
   ) %>%
   arrange(match(cat, cat_order)) %>%
@@ -145,12 +166,12 @@ knitr::kable(cat_count_comp %>%
 knitr::kable(
   weighted_output %>%
     filter(
-      gender == "female" &
-        educ == "no_HS" &
-        ethnic == "hispanic" &
+      gender == "male" &
+        educ == "some_college" &
+        ethnic == "white" &
         region == "northeast"
     ) %>%
-    select(-age_range,-clin_status,-(i01_w:i50_w),-TOT_raw_weight) %>%
+    select(-Noweight,-age_combined,-(i01_w:i44_w),-TOT_raw_weight) %>%
     sample_n(1),
   digits = 2,
   caption = "Table 2: Demographic multiplier from under-sampled cell"
@@ -161,14 +182,17 @@ knitr::kable(
     filter(
       gender == "female" &
         educ == "HS_grad" &
-        ethnic == "black" &
+        ethnic == "hispanic" &
         region == "west"
     ) %>%
-    select(-age_range,-clin_status,-(i01_w:i50_w),-TOT_raw_weight) %>%
+    select(-Noweight,-age_combined,-(i01_w:i44_w),-TOT_raw_weight) %>%
     sample_n(1),
   digits = 2,
   caption = "Table 3: Demographic mupltiplier from accurately sampled cell"
 )
+
+
+# #######START HERE
 
 knitr::kable(
   tail(input_demo_wts) %>%
