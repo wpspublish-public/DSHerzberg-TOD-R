61271db3c15bd160ad8555af905f088ce62f35ac
David Herzberg <dherzberg@wpspublish.com>
2021-10-18 16:06:37 -0700

    begin to grapple with ORF recoding

--- CODE/NORMS/TOD-raw-to-ss-lookup.R
+++ CODE/NORMS/TOD-raw-to-ss-lookup.R
@@ -22,7 +22,7 @@ scores <- c("sege_sum", "rlne_sum", "rhme_sum", "snwe_sum",
 
 # Tokens setting the specific score to be normed on this iteration of the
 # script.
-score_to_norm_stem <- "rhme_sum"
+score_to_norm_stem <- "ORF_noNeg"
 score_to_norm_file_name <- str_c(score_to_norm_stem, "-norms-input.csv")
 score_to_norm_max_raw <- data.frame(test = score_to_norm_stem) %>%
   mutate(
@@ -34,11 +34,10 @@ score_to_norm_max_raw <- data.frame(test = score_to_norm_stem) %>%
       test == "rhme" ~ 30,
       test == "lske" ~ 33,
       test == "snwe" ~ 32,
-      test == "ORF_noNeg" ~ 163
+      test == "ORF_noNeg" ~ 263
     )
   ) %>%
   pull(max_raw)
-
 
 # to use age as predictor in cNORM, read in DOB, date_admin, calculate
 # chronological age as decimal value.
@@ -49,45 +48,17 @@ age_contin <- suppressMessages(read_csv(here(
     across(
       c(DOB, admin_date),
       ~
-        # lubridate::mdy() coerces string into date-time type
         mdy(.x)
     ),
-    # [start_date] %--% [end_date] uses lubridate operator %--% to create a time
-    # interval (duration). Dividing interval by a period (years(1) = 1 year)
-    # returns age in years-months-days as a decimal value (e.g., 5 years, 6
-    # months, 0 days = 5.5)
     age = (DOB %--% admin_date) / years (1)
   ) %>%
-  # here bind_cols joins a col created by cNORM::getGroups(), which returns vec
-  # with equal size age strata, where value of col is a label for the age stratum that
-  # each row belongs to, and that label is the arithmetic mean of chronological
-  # age within that group
   bind_cols(getGroups(.$age)) %>% 
   rename(group = ...51) %>% 
   select(ID, age, group)
 
-# write versions of age, group vars for consultant Alex Lenhard.
-age_contin %>% 
-  select(ID, age) %>% 
-  write_csv(here(
-    str_c(input_file_path, "TOD-AL-age-contin.csv")
-  ))
-
-age_contin %>% 
-  rename(getGroup = group) %>% 
-  select(ID, age, getGroup) %>% 
-  write_csv(here(
-    str_c(input_file_path, "TOD-AL-age-contin-getGroup.csv")
-  ))
-
-
 # Next block reads an input containing multiple raw score columns per person,
 # processes into separate dfs that are input files into cNORM for norming one
-# raw score. Within the process, a list of dfs is created and those dfs are
-# eventually written out as .csvs, but the list itself is invisible, it's never
-# preserved in the global environment. These new input files for cnorm()
-# incorporate the continuous age variable and age grouping variable created in
-# the previous step.
+# raw score. 
 map(
   scores,
   ~
@@ -115,7 +86,7 @@ input <- suppressMessages(read_csv(here(str_c(
   input_file_path, score_to_norm_file_name
 ))))
 
-# Alex Lenhard's recommended approach
+# Alex Lenhard's recommended approach with cNORM
 
 # Use the all-in-one cnorm() function to create the model.
 # Compare diagnostics from two approaches to defining age groups: -
@@ -130,27 +101,24 @@ input <- suppressMessages(read_csv(here(str_c(
 # curves. With plot(model, "series"), you can use "end" argument to set upper
 # limit of predictors.
 
-model <- cnorm(raw = input$raw, group = input$group, k = 4, terms = 5, scale = "IQ")
+model <- cnorm(raw = input$raw, group = input$group, k = 4, terms = 3, scale = "IQ")
 # model <- cnorm(raw = input$raw, age = input$age, width = 1, k = 4, terms = 4, scale = "IQ")
 plot(model, "series", end = 10)
 checkConsistency(model)
 
-#Once you have the model, cNORM allows you generate post hoc age groups of any
-#width, centers on any age points you choose. The tab_names token is used for
-#writing out a multi-tabbed norms table, the labels in this token give the lower
-#bound of each age range.
-tab_names <- c("5.0", "5.6", "6.0", "6.6", "7.0", "7.6", "8.0")
+# Token for names of output age groups
+tab_names <- c("5:0-5:3", "5:4-5:7", "5:8-5:11", "6:0-6:5", "6:6-6:11", "7:0-9:3")
 
 # Prepare a list of data frames, each df is raw-to-ss lookup table for an age group.
 norms_list <- rawTable(
-  c(5.25, 5.75, 6.25, 6.75, 7.25, 7.75, 8.25), 
+  c(5.167, 5.5, 5.833, 6.25, 6.75, 8.167), 
   model, 
   step = 1, 
   minNorm = 40, 
   maxNorm = 130, 
   minRaw = 1, 
   maxRaw = score_to_norm_max_raw,
-  pretty = TRUE
+  pretty = FALSE
   ) %>% 
   set_names(tab_names) %>% 
   map( 
@@ -175,21 +143,11 @@ reversal_report <- norms_list %>%
   ))
 
 
-# Write raw-to-ss lookups by agestrat into tabbed, xlsx workbook. To create
-# named tabs, supply writexl::write_xlsx() with a named list of dfs for each
-# tab, tab names will be the names of the list elements
+# Write raw-to-ss lookups by agestrat into tabbed, xlsx workbook.
 write_xlsx(norms_list,
            here(str_c(
              output_file_path, score_to_norm_stem, "-raw-ss-lookup-tabbed.xlsx"
            )))
-
-# write model summary to text file, so you can replicate model later.
-capture.output(
-  summary(model),
-  file = here(
-    str_c(output_file_path, score_to_norm_stem, "-model-summ.txt")  )
-)
-
 
 # write raw-to-ss-lookups to single-sheet table
 table <- norms_list %>%
