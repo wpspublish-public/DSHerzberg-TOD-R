36ab110c1aefe24535791d0a0671c74a7652f2be
David Herzberg <dherzberg@wpspublish.com>
2021-10-25 14:21:28 -0700

    write out weighted and unweighted POM scores

---
+++ CODE/DATA-WEIGHTING/TODE_8.27.21_fornorms-weighting-data-pom-code.R
@@ -0,0 +1,304 @@
+suppressMessages(library(here))
+suppressMessages(suppressWarnings(library(tidyverse)))
+suppressMessages(suppressWarnings(library(survey)))
+
+set.seed(123)
+
+gender <- c("male", "female")
+educ <- c("no_HS", "HS_grad", "some_college", "BA_plus")
+ethnic <- c("hispanic", "asian", "black", "white", "other")
+region <- c("northeast", "south", "midwest", "west")
+
+var_order <- c("agestrat", "gradestrat", "gender", "educ", "ethnic", "region")
+
+var_order_census_match  <- c("gender", "educ", "ethnic", "region")
+
+cat_order <- c(
+  NA, "5:0-5:7", "5:8-5:11", "6:0-6:3", "6:4-6:7", "6:8-6:11", "7:0-7:5", 
+  "7:6-7:11", "8:0-9:3",
+  NA, "K_Fall", "K_Spring", "1_Fall", "1_Spring", "2_Fall", "2_Spring", 
+  NA, "male", "female",
+  NA, "no_HS", "HS_grad", "some_college", "BA_plus",
+  NA, "hispanic", "asian", "black", "white", "other",
+  NA, "northeast", "south", "midwest", "west")
+
+last_item <- 32
+
+file_name <- "TODE_8.27.21_fornorms-"
+
+input_file_path <- "INPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
+output_file_path <- "OUTPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
+fileName_path   <- "TODE_8.27.21_fornorms.csv"
+
+original_input <- suppressMessages(read_csv(
+  str_c(input_file_path, fileName_path)
+)) %>% rename(
+  gender = Gender,
+  educ = SES,
+  ethnic = Ethnicity,
+) %>% 
+  mutate(
+    across(gender, ~ case_when(
+      . == 1 ~ "male",
+      . == 2 ~ "female")),
+    across(educ, ~ case_when(
+      . == 1 ~ "no_HS",
+      . == 2 ~ "HS_grad",
+      . == 3 ~ "some_college",
+      . == 4 ~ "BA_plus")),
+    across(ethnic, ~ case_when(
+      . == 1 ~ "asian",
+      . == 2 ~ "black",
+      . == 3 ~ "white",
+      . %in% c(4, 5, 6) ~ "other",
+      . == 7 ~ "hispanic")),
+    across(region, ~ case_when(
+      . == 1 ~ "northeast",
+      . == 2 ~ "midwest",
+      . == 3 ~ "south",
+      . == 4 ~ "west")),
+    sege_pom = sege_sum/25, 
+    rlne_pom = rlne_sum/120, 
+    rhme_pom = rhme_sum/30, 
+    snwe_pom = snwe_sum/32, 
+    lswe_pom = lswe_sum/38, 
+    lske_pom = lske_sum/33, 
+  )
+  # ) %>% 
+  # select(-(sege_sum:i32))
+
+fileName_path   <- "census_pct.csv"
+
+census_match_cat_count <- suppressMessages(read_csv(
+  str_c(input_file_path, fileName_path)
+)) %>% 
+  mutate(n_census = round(nrow(original_input)*(pct_census/100), 0))
+
+census_match_pct_wide <- suppressMessages(read_csv(
+  str_c(input_file_path, fileName_path)
+)) %>%
+  select(-var) %>%
+  pivot_wider(names_from = cat,
+              values_from = pct_census)
+
+var_order_census_match %>%
+  map(
+    ~ census_match_cat_count %>%
+      filter(var == all_of(.x)) %>%
+      select(-var, -pct_census) %>%
+      rename(!!.x := cat, Freq = n_census)
+  ) %>%
+  setNames(str_c(var_order_census_match, "_census")) %>%
+  list2env(envir = .GlobalEnv) %>% 
+  invisible(.)
+
+unweighted_survey_object <- svydesign(ids = ~1, 
+                                      data = original_input, 
+                                      weights = NULL)
+
+rake_original_input <- rake(design = unweighted_survey_object,
+                              sample.margins = list(~gender, ~educ, ~ethnic, ~region),
+                              population.margins = list(gender_census, educ_census, 
+                                                        ethnic_census, region_census))
+
+input_demo_wts <- bind_cols(
+  rake_original_input[["variables"]],  
+  data.frame(rake_original_input[["prob"]]), 
+  data.frame(demo_wt = weights(rake_original_input))
+) %>% 
+  rename(samp_prob = rake_original_input...prob...) %>% 
+  mutate(ratio = samp_prob / demo_wt) %>% 
+  select(ID:region, samp_prob, demo_wt, ratio, everything()) %>% 
+  arrange(desc(samp_prob))
+
+rm(list = ls(pattern = "object|rake"))
+
+unweighted_output <- input_demo_wts %>%
+  select(-c(samp_prob, ratio)) %>%
+  rename_with(~ str_c("i", str_pad(
+    as.character(1:last_item), 2, side = "left", pad = "0"), "_uw"),
+    i01:!!sym(str_c("i", last_item))) %>%
+  mutate(
+    TOT_raw_unweight = rowSums(.[grep("*_uw", names(.))]
+  )) %>%
+  relocate(TOT_raw_unweight, .after = demo_wt)
+
+write_csv(unweighted_output,
+          here(
+            str_c(
+              output_file_path,
+              file_name,
+              "unweighted-data-for-analysis.csv"
+            )
+          ),
+          na = "")
+
+weighted_output <- original_input %>%
+  left_join(unweighted_output[c("ID", "demo_wt")], by = "ID") %>%
+  rename_with(~ str_c("i", str_pad(
+    as.character(1:last_item), 2, side = "left", pad = "0"
+  ), "_w"),
+  i01:!!sym(str_c("i", last_item))) %>%
+  mutate(across(c(i01_w:!!sym(str_c("i", last_item, "_w"))),
+                ~ . * demo_wt)) %>%
+  mutate(
+    TOT_raw_weight = rowSums(.[grep("*_w$", names(.))]
+    )) %>%
+  relocate(demo_wt, TOT_raw_weight, .before = i01_w)
+
+write_csv(weighted_output,
+          here(
+            str_c(
+              output_file_path,
+              file_name,
+              "weighted-data-for-analysis.csv"
+            )
+          ),
+          na = "")
+
+demo_weight_by_crossing_input <- weighted_output %>%
+  group_by(demo_wt) %>%
+  summarize(
+    gender = first(gender),
+    educ = first(educ),
+    ethnic = first(ethnic),
+    n_input = n(),
+    region = first(region)
+  ) %>%
+  relocate(c(demo_wt, n_input), .after = region) %>%
+  arrange(
+    match(gender, cat_order),
+    match(educ, cat_order),
+    match(ethnic, cat_order),
+    match(region, cat_order)
+  )
+
+write_csv(demo_weight_by_crossing_input,
+          here(
+            str_c(
+              output_file_path,
+              file_name,
+              "weights-per-demo-crossing-input.csv"
+            )
+          ),
+          na = "")
+
+demo_crossings_all <- expand_grid(
+  gender = gender,
+  educ = educ,
+  ethnic = ethnic,
+  region = region
+)
+
+demo_weight_by_crossing_all <- demo_crossings_all %>% 
+  left_join(demo_weight_by_crossing_input, by = c("gender", "educ", "ethnic", "region")) %>% 
+  bind_cols(
+    census_match_pct_wide
+  ) %>% 
+  mutate(
+    pct_census_gender = case_when(
+      gender == "male" ~ male,
+      TRUE ~ female
+    ), 
+    pct_census_educ = case_when(
+      educ == "no_HS" ~ no_HS,
+      educ == "HS_grad" ~ HS_grad,
+      educ == "some_college" ~ some_college,
+      TRUE ~ BA_plus
+    ), 
+    pct_census_ethnic = case_when(
+      ethnic == "hispanic" ~ hispanic,
+      ethnic == "asian" ~ asian,
+      ethnic == "black" ~ black,
+      ethnic == "white" ~ white,
+      TRUE ~ other
+    ), 
+    pct_census_region = case_when(
+      region == "northeast" ~ northeast,
+      region == "south" ~ south,
+      region == "midwest" ~ midwest,
+      TRUE ~ west
+    ), 
+    across(
+      c(
+        pct_census_gender,
+        pct_census_educ,
+        pct_census_ethnic,
+        pct_census_region,
+      ),
+      ~
+        . * .01
+    ),
+    n_census = round(
+      (pct_census_gender * pct_census_educ * 
+         pct_census_ethnic * pct_census_region) *
+        nrow(original_input), 
+      2
+    )
+  ) %>% 
+  select(gender:n_input, n_census)
+
+write_csv(demo_weight_by_crossing_all,
+          here(
+            str_c(
+              output_file_path,
+              file_name,
+              "weights-per-demo-crossing-all.csv"
+            )
+          ),
+          na = "")
+
+weighted_pom_scores <- original_input %>%
+  # mutate(ORF_noNeg = ORF + 100) %>%
+  select(-ORF,-(i01:!!sym(str_c("i", last_item)))) %>%
+  left_join(demo_weight_by_crossing_input,
+            by = c("gender", "educ", "ethnic", "region")) %>%
+  # relocate(agestrat:gradestrat, .before = gender) %>%
+  mutate(
+    sege_pom_w = sege_pom,
+    rlne_pom_w = rlne_pom,
+    rhme_pom_w = rhme_pom,
+    snwe_pom_w = snwe_pom,
+    lswe_pom_w = lswe_pom,
+    lske_pom_w = lske_pom
+    # ORF_noNeg_w = ORF_noNeg
+  ) %>%
+  mutate(across(c(sege_pom_w:lske_pom_w),
+                ~
+                  case_when(
+                    is.na(noweight) ~ round(. * demo_wt, 0),
+                    TRUE ~ .
+                    )
+                )
+         ) %>%
+  select(ID:region, demo_wt, 
+         sege_pom, sege_pom_w,
+         rlne_pom, rlne_pom_w,
+         rhme_pom, rhme_pom_w,
+         snwe_pom, snwe_pom_w,
+         lswe_pom, lswe_pom_w,
+         lske_pom, lske_pom_w
+         # ORF_noNeg, ORF_noNeg_w
+  )
+
+# write weighted_pom_scores twice, because it is both output from weighting, and
+# input to norming.
+write_csv(weighted_pom_scores,
+          here(
+            str_c(
+              input_file_path,
+              file_name,
+              "weighted-pom-scores.csv"
+            )
+          ),
+          na = "")
+write_csv(weighted_pom_scores,
+          here(
+            str_c(
+              output_file_path,
+              file_name,
+              "weighted-pom-scores.csv"
+            )
+          ),
+          na = "")
+
--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,121 +1,22 @@
-age5 <- data.frame(
-  raw = c(5, 4, 3, 2, 1), 
-  ss = c(120, 110, 100, 90, 80)
-)
-age6 <- data.frame(
-  raw = c(5, 4, 3, 2, 1), 
-  ss = c(117, 107, 97, 87, 77)
-)
-age7 <- data.frame(
-  raw = c(5, 4, 3, 2, 1), 
-  ss = c(115, 105, 95, 85, 75)
-)
+library(cNORM)
+suppressMessages(suppressWarnings(library(tidyverse)))
+suppressMessages(library(here))
+library(writexl)
+suppressMessages(library(lubridate))
 
-lookup_list <- lst(age5, age6, age7)
+combined_score_to_norm_file_name <- "TODE_8.27.21_fornorms-weighted-sum-scores.csv"
+input_file_path <- "INPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
+output_file_path <- "OUTPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
 
+# tokens for score names
+scores <- c("sege_sum", "rlne_sum", "rhme_sum", "snwe_sum",
+            "lswe_sum", "lske_sum", "ORF_noNeg")
 
-ss_range <- 90:110
-
-lookup_combo <- map(
-  ss_range,
-  ~
-    getNormCurve(.x, model, step = .5) %>% 
-    mutate(
-      across(
-        raw,
-        ~
-          round(., 0)
-      )
-    ) %>% 
-    select(age, raw) %>% 
-    rename(!!sym(str_c("ss", .x)) := raw) 
-) %>% 
-  reduce(
-  left_join,
-  by = "age"
-) %>% 
-  pivot_longer(-age, names_to = "ss", values_to = "raw") 
-
-ggplot(lookup_combo, aes(age, raw)) +
-  geom_line(
-    aes(
-      color = ss
-    )
-  )
-
-
-urlRemote_path  <- "https://raw.github.com/"
-github_path <- "wpspublish/DSHerzberg-TOD-R/master/INPUT-FILES/NORMS/TODE_8.27.21_fornorms/"
-input_file_name <- "input-TOD-cNORM-reprex1.csv"
-
-input <- suppressMessages(read_csv(url(
-  paste0(urlRemote_path, github_path, input_file_name)
-)))
-
-model <- cnorm(raw = input$raw, group = input$group, k = 4, terms = 4, scale = "IQ")
-
-tab_names <- c(5.00, 5.67, 6.00, 6.33, 6.67, 7.00, 7.50, 8.00)
-
-norms_combo_table <- rawTable(
-  c(5.00, 5.67, 6.00, 6.33, 6.67, 7.00, 7.50, 8.00),
-  model,
-  step = 1,
-  minNorm = 40,
-  maxNorm = 160,
-  minRaw = 1,
-  maxRaw = 26,
-  pretty = FALSE
-) %>%
-  map(~
-        select(.x, raw, norm) %>%
-        summarize(raw = raw,
-                  ss = round(norm, 0))) %>%
-  reduce(left_join,
-         by = "raw") %>% 
-  set_names("raw", tab_names) %>% 
-  pivot_longer(-raw, names_to = "agestrat", values_to = "ss") %>% 
-  group_by(raw) %>% 
-  mutate(
-    reversal = case_when(
-      lag(ss) < ss ~ 1
-    )
-  )
-
-reversal_report <- norms_combo_table %>% 
-  filter(reversal == 1) %>% 
-  select(raw, agestrat)
-
-
-
-# Write assignments by coder into tabbed, xlsx workbook. To create named tabs,
-# supply writexl::write_xlsx() with a named list of dfs for each tab, tab names
-# will be the names of the list elements
-write_xlsx(norms_list_temp,
-           here(str_c(
-             "temp-raw-ss-lookup.xlsx"
-           )))
-
-
-age_contin <- suppressMessages(read_csv(here(
-  str_c(input_file_path, "TODE_8.27.21_fornorms.csv")
-)))
-
-
-temp1 <- age_contin %>% filter(ID %in% c(234032, 234033, 234034, 234035, 262027))
-
-
-sink(h)
-i <- 1:10
-outer(i, i, "*")
-sink()
-
-capture.output(
-  summary(model),
-  file = here(
-    str_c(output_file_path, input_file_stem, "-model-summ.txt")  )
-)
-
-max_raw <- data.frame(test = "lswe") %>%
+# Tokens setting the specific score to be normed on this iteration of the
+# script.
+score_to_norm_stem <- "sege_sum"
+score_to_norm_file_name <- str_c(score_to_norm_stem, "-norms-input.csv")
+score_to_norm_max_raw <- data.frame(test = score_to_norm_stem) %>%
   mutate(
     max_raw = case_when(
       str_detect(test, "sege") ~ 25,
@@ -129,58 +30,17 @@ max_raw <- data.frame(test = "lswe") %>%
   ) %>%
   pull(max_raw)
 
-max_raw <- temp2$max_raw
+input <- suppressMessages(read_csv(here(str_c(
+  input_file_path, score_to_norm_file_name
+)))) %>%
+  mutate(raw_prop = raw/25)
 
-# lske = 33
-# orf = each grade is given a different passage to read within 1 minute and 
-# it is unlikely anyone would read the whole thing with zero errors, 
-# but the total max for each passage is: Kinder 114, 1st grade fall = 119, 
-# 1st grade spring = 112, 2nd grade = 163
+score_to_norm_stem_w <- "sege_sum_w"
+score_to_norm_file_name_w <- str_c(score_to_norm_stem_w, "-norms-input.csv")
 
-temp2 <- suppressMessages(read_csv(here(
-  str_c(input_file_path, combined_score_to_norm_file_name)
-)))
-
-model_w <- cnorm(
-  raw = input$raw, 
-  group = input$group, 
-  weights = input$demo_wt, 
-  k = 4, 
-  terms = 4, 
-  scale = "IQ"
-  )
-# plot(model_w, "series", end = 10)
-# checkConsistency(model_w)
-
-
-# Prepare a list of data frames, each df is raw-to-ss lookup table for an age group.
-norms_list_w <- rawTable(
-  c(5.167, 5.5, 5.833, 6.25, 6.75, 8.167), 
-  model_w, 
-  step = 1, 
-  minNorm = 40, 
-  maxNorm = 130, 
-  minRaw = 1, 
-  maxRaw = score_to_norm_max_raw,
-  pretty = FALSE
-) %>% 
-  set_names(tab_names) %>% 
-  map( 
-    ~
-      select(.x, raw, norm) %>% 
-      summarize(raw = raw,
-                ss = round(norm, 0))
-  )
-
-# write raw-to-ss-lookups to single-sheet table
-table_w <- norms_list_w %>%
-  reduce(left_join,
-         by = "raw") %>%
-  set_names("raw", tab_names)
-
-write_csv(table_w, 
-          here(
-            str_c(output_file_path, score_to_norm_stem, "-raw-ss-lookup-table-w.csv")
-          ))
+input_w <- suppressMessages(read_csv(here(str_c(
+  input_file_path, score_to_norm_file_name_w
+  )))) %>%
+  mutate(raw_prop = raw/34)
 
 
