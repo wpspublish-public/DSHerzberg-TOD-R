5c942a8401405b38149b7ac5883c14f51d4d75d8
David Herzberg <dherzberg@wpspublish.com>
2022-02-02 17:24:35 -0800

    TODE print format transformation for hand-smoothed norms.

--- CODE/NORMS/scratch.R
+++ CODE/NORMS/scratch.R
@@ -1,88 +1,24 @@
-library(psych)
-descriptives <- data.frame(input) %>% 
-  filter(age < 6) %>% 
-  select(raw) %>%
-  describe(fast = T) %>% 
-  rownames_to_column(var = "scale")
+suppressMessages(library(here))
+suppressMessages(library(tidyverse))
 
-hist_data <- data.frame(input) %>% 
-  filter(age < 6) %>% 
-  select(raw)
-  
-  
+file_name <- "Letter and Sight word Recognition age.csv"
+input_file_path <- "PRINT-FORMAT-NORMS-TABLES/POST-cNORM-HAND-SMOOTHED-TABLES/"
+# output_file_path <- "OUTPUT-FILES/NORMS/TODC_final_11.17.21_adult_fornorms/"
 
-hist(hist_data$raw, 
-     main="rhme: 5:0-5:11",
-     freq = TRUE,
-     xlab="raw score", 
-     border="blue", 
-     col="green",
-     xlim=c(0,30),
-     las=1, 
-     breaks=30)
+input <- suppressMessages(read_csv(here(str_c(
+  input_file_path, file_name
+))))
 
-hist(input$age, 
-     main="TOD-S age for qrf_sum",
-     freq = TRUE,
-     xlab="age", 
-     border="blue", 
-     col="green",
-     xlim=c(0, 20),
-     las=1, 
-     breaks=20)
+perc_ss_cols <- suppressMessages(read_csv(here(
+  "PRINT-FORMAT-NORMS-TABLES/perc-ss-cols.csv"
+)))
 
+print_lookup_50 <- input %>% 
+  select(raw, `5.0-5.3`) %>% 
+  rename(ss = `5.0-5.3`) %>%
+  right_join(perc_ss_cols, by = "ss") %>% 
+  arrange(desc(ss)) %>% 
+  select(perc, ss, raw)
 
-
-temp1 <- weighted_sum_scores %>% 
-  select(ID, sege_sum, sege_sum_w_pom) %>% 
-  rename(sege_sum_w = sege_sum_w_pom) %>% 
-  drop_na() %>% 
-  mutate(across(
-    sege_sum_w,
-    ~ round(., 0)
-    ))
-
-unique(temp1$sege_sum_w)
-
-temp3 %>% 
-  select(-ID) %>% 
-  describe(fast = TRUE)
-
-write_csv(
-  temp1,
-  "/Users/dherzberg/Desktop/R/WEIGHTING-DATA/INPUT-FILES/example-rescale-weighted-raw-scores-input.csv"
-)
-
-temp1 <- read_csv(here(
-  str_c(input_file_path, combined_score_to_norm_file_name)
-))
-
-n_age_group <- age_contin %>% 
-  group_by(group) %>% 
-  count(group)
-"5.0-5.3", "5.4-5.7", "5.8-5.11"
-
-temp2 <- temp1 %>% 
-  filter(!is.na(qrf_sum))
-
-write_csv(temp2,
-          here(
-            str_c(input_file_path, "TODC_final_gr1_12_10.28.21_fornorms-pflsum1.csv")
-          ))
-
-# gradestrat for pflsum1
-# 3  4  5  6  7  8  9 10 11 12
-
-# gradestrat for pflsum2
-# 13 14 15 16 17 18 19 20 21 22 23 24 25 26
-
-temp_out <- temp1 %>% 
-  filter(is.na(Noweight)) %>% 
-  select(-Noweight)
-
-sort(unique(temp_out$Age))
-
-write_csv(temp_out,
-          here(
-            str_c(input_file_path, "TOD-psychometrica.csv")
-          ))
+##### NEXT STEP: TRANSFORM INPUT INTO A LIST OF DFS WITH IDENTICAL FORMAT 
+# TO norms_list ON CNORM SCRIPT
