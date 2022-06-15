ef9b3e03bf40343fa314439e28e1da962139a25a
David Herzberg <dherzberg@wpspublish.com>
2022-06-15 08:58:58 -0700

    TODS composite norms

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,12 +1,25 @@
-a <- round(rnorm(500, mean = 50, sd = 10), 0)
-b <- round(rnorm(500, mean = 60, sd = 11), 0)
-data <- list(a, b)
+set.seed(12345)
+a <- rnorm(500, mean = 50, sd = 10)
+set.seed(12345)
+b <- rnorm(500, mean = 50, sd = 10)
+identical(a, b)
 
-norms <- map(
-  data,
+library(tidyverse)
+
+means <- c(40, 50, 60)
+sds <- c(9, 10, 11)
+
+set.seed(12345)
+data1 <- map2(
+  means,
+  sds,
   ~
-    bestNormalize(.x) %>%
-    pluck("chosen_transform") %>% 
-    class() %>% 
-    pluck(1)
-)
+    rnorm(500, mean = .x, sd = .y)
+    )
+
+
+
+temp1 <- flatten(list(30:60, rep(list(20:40), 3)))
+temp2 <- flatten(list(30:60, 20:40, 20:40, 20:40))
+identical(temp1, temp2)
+
