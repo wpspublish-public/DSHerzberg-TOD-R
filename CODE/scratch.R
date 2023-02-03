8f03e126e95fbaace27fa759571bd529f70bf85e
David Herzberg <dherzberg@wpspublish.com>
2023-02-02 16:46:54 -0800

    read_csv with map() structure

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,23 +1,49 @@
 suppressMessages(library(here))
 suppressMessages(library(tidyverse))
-suppressMessages(library(readxl))
+
+scores <- c("DRIQ", "DRIW", "LWC", "PV", "QRF", "WRF")
+norm_groups <- c("age", "grade")
+lookup_names <- cross2(scores, norm_groups) %>% 
+  map_chr(str_c, collapse = "-")
 
 
-temp1 <- read_excel(
+temp1 <- suppressMessages(
+  read_csv(
   here(
-    "INPUT-FILES/OES-INPUT-TABLES/TOD-S/PV ability to standard score-age.xlsx"
+    "INPUT-FILES/OES-INPUT-TABLES/TOD-S/PV-age-lookup.csv"
   )
-)
-
-
-temp2 <- temp1 %>% 
+)) %>% 
 pivot_longer(
   cols = -raw,
-  names_to = "age_strat",
+  names_to = "age_grade",
   values_to = "ss"
-)
+) %>% 
+  mutate(
+    test = "PV",
+    norm_group = "age"
+  ) %>% 
+  select(norm_group, test, age_grade, raw, ss)
   
 
 
 
+temp2 <- map(lookup_names,
+             ~
+                     suppressMessages(read_csv(here(
+                       str_c(
+                         "INPUT-FILES/OES-INPUT-TABLES/TOD-S/",
+                         .x,
+                         "-lookup.csv"
+                       )
+                     )))) %>%
+  set_names(lookup_names)
 
+comp1 <- temp3[[4]]
+
+comp2 <- suppressMessages(
+  read_csv(
+    here(
+      "INPUT-FILES/OES-INPUT-TABLES/TOD-S/PV-age-lookup.csv"
+    )
+  )
+)
